80fec7bdb7b4e784eed64c0509502ffbb80f9689
David Lu <david.lu@hotmail.com>
2015-11-14 20:39:07 -0500

    added menu crawler

---
+++ crawlmenu.php
@@ -0,0 +1,68 @@
+<html>
+
+<head>
+	<title>WatChew Database Crawler For Weekly Menu</title>
+</head>
+
+<body>
+	<?php
+		//MySQL
+		$servername = "localhost";
+		$username = "watchew";
+		$password = "enghacks";
+		$dbname = "nutrition";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		if($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		//echo "Connected suessfully.<br>";
+
+		//Clear DB
+		$clearDB = 1;
+		if($clearDB) {
+			if($conn->query("delete from menu;") === TRUE){
+				echo "Table menu cleared. <br>";
+			}
+			else {
+				echo "Error: delete from food;" . "<br>" .$conn->error;
+			}
+		}
+
+		function crawl_page($url, $conn)
+		{
+			//echo "Crawling: " . $url . "<br>";
+		    $seen[$url] = true;
+
+		    $dom = new DOMDocument('1.0');
+		    @$dom->loadHTMLFile($url);
+
+		    //Get nutrition information from document
+		    $links= $dom->getElementsByTagName('a');
+		    foreach($links as $item) {
+		    	$cur = $item->getAttribute('src');
+
+		    	if(strpos($cur, "https://uwaterloo.ca/food-services/menu/product/") != FALSE){
+		    		$cur = filter_var($cur, FILTER_SANITIZE_NUMBER_INT);
+		    	}
+
+			    $sql = "INSERT INTO menu (id) VALUES ('$cur');";
+				echo "Querying DB with: " . $sql . "<br>";
+				if($conn->query($sql) === TRUE) {
+					echo "Query sucessful, URL: ". $url . " crawled.<br>";
+				} else {
+					echo "Error: " . $sql . "<br>" .$conn->error;
+				}
+			}
+		}
+		crawl_page("https://uwaterloo.ca/food-services/menu" . $x, $conn);
+
+		$conn->close();
+		echo "Crawl complete, " . ($max_id - $min_id) + 1 . " entries crawled.<br>";
+		echo "Have a nice day!<br>";
+		echo "<img src='surprise.jpg'>";
+	?>
+</body>
+
+</html>
